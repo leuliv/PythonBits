37fa72edbb294397ecafbf5ac87b8969fec7d0e6
Leul Woldegabriel <leulwoldegabriel@gmail.com>
2020-09-19 13:12:19 +0300

    Update send.py

--- SendLyrics/send.py
+++ SendLyrics/send.py
@@ -1,10 +1,11 @@
 from telethon import TelegramClient
 
-api_id = "1423014"
-api_hash = "f8bcf807bdf6e38aab1231a382f5a4de"
+#Get app_id and api_hash in the telegram api site
+api_id = "app_id"
+api_hash = "api_hash"
 
-phone = "+251944228847"
-session_file = 'leulw'
+phone = "phone_number" #Your Phone Number here
+session_file = 'session_name' #choose a unique session name
 
 text_file = open("lyrics.txt", "r")
 print(len(text_file.readlines()))
@@ -12,6 +13,6 @@ print(len(text_file.readlines()))
 with TelegramClient(session_file, api_id, api_hash,
                         sequential_updates=True) as client:
     for line in text_file.readlines():
-        client.loop.run_until_complete(client.send_message('i_trustmyself', line))
+        client.loop.run_until_complete(client.send_message('reciever_uname', line)) #replace 'reciever_id' with recievers username 
 
 text_file.close()
